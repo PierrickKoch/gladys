8b51242f8cf4790c78f8666ff1c5b698af5b5851
Pierrick Koch <pierrick.koch@gmail.com>
2013-07-01 17:29:54 +0200

    [test] use BOOST_AUTO_TEST_CASE(namespace)
    and BOOST_TEST_MESSAGE() - use --log_level=test_suite command line
    argument to display messages.
    and remove useless boost include/link in cmake

--- test/test_gladys.cpp
+++ test/test_gladys.cpp
@@ -13,19 +13,22 @@
 #include <string>
 #include <sstream>
 
+#include "gladys/gdal.hpp"
 #include "gladys/gladys.hpp"
 #include "gladys/nav_graph.hpp"
+
+BOOST_AUTO_TEST_SUITE( gladys )
 
 BOOST_AUTO_TEST_CASE( test_gdal )
 {
     std::string path = "/tmp/test_gladys_gdal.tif";
-    gladys::gdal gdal_to_file;
+    gdal gdal_to_file;
     gdal_to_file.set_size(5, 320, 240);
     gdal_to_file.set_utm(31);
-    gdal_to_file.set_transform(123, 456, 0.5, 0.5);
+    gdal_to_file.set_transform(123, 456, 0.4, 0.6);
     gdal_to_file.save(path);
     // load the file we just saved
-    gladys::gdal gdal_from_file(path);
+    gdal gdal_from_file(path);
 
     BOOST_CHECK_EQUAL( gdal_from_file.get_x(),          gdal_to_file.get_x() );
     BOOST_CHECK_EQUAL( gdal_from_file.get_y(),          gdal_to_file.get_y() );
@@ -34,34 +37,36 @@ BOOST_AUTO_TEST_CASE( test_gdal )
     BOOST_CHECK_EQUAL( gdal_from_file.get_utm_pose_x(), gdal_to_file.get_utm_pose_x() );
     BOOST_CHECK_EQUAL( gdal_from_file.get_utm_pose_y(), gdal_to_file.get_utm_pose_y() );
     BOOST_CHECK_EQUAL( gdal_from_file.bands.size(),     gdal_to_file.bands.size() );
-    std::cout<<"GDAL OK"<<std::endl;
+    BOOST_TEST_MESSAGE( "GDAL OK" );
 }
 
 BOOST_AUTO_TEST_CASE( test_write_graphviz )
 {
-    gladys::nav_graph obj;
+    nav_graph obj;
     obj.get_vertex(1,2);
     obj.get_vertex(2,1);
     obj.get_vertex(3,2);
     std::ostringstream oss_graphviz;
     obj.write_graphviz(oss_graphviz);
-    std::cout<<"oss_graphviz.size() = "<<oss_graphviz.str().size()<<std::endl;
+    BOOST_TEST_MESSAGE( "oss_graphviz.size() = " << oss_graphviz.str().size() );
     BOOST_CHECK_EQUAL( oss_graphviz.str().size() , 102 );
 }
 
 BOOST_AUTO_TEST_CASE( test_raster_to_graph )
 {
     std::string path = "/tmp/test_gladys_raster_to_graph.tif";
-    gladys::gdal tmp;
+    gdal tmp;
     tmp.set_size(5, 4, 4);
     tmp.save(path);
-    gladys::nav_graph ng(path);
+    nav_graph ng(path);
     std::ostringstream oss_graphviz;
     ng.write_graphviz(oss_graphviz);
-    std::cout<<"oss_graphviz.size() = "<<oss_graphviz.str().size()<<std::endl;
+    BOOST_TEST_MESSAGE( "oss_graphviz.size() = " << oss_graphviz.str().size() );
     BOOST_CHECK_EQUAL( oss_graphviz.str().size() , 2066 );
     ng.save("/tmp/test_gladys_raster_to_graph_nav.tif");
 
-    gladys::gladys obj(path, "/tmp/TODO");
+    gladys obj(path, "/tmp/TODO");
 }
 
+
+BOOST_AUTO_TEST_SUITE_END();
