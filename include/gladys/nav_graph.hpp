1ef30ea175be23696c6eb77c93604ece3f07c387
Pierrick Koch <pierrick.koch@gmail.com>
2013-07-04 12:32:32 +0200

    [nav] add get_closest_vertex

--- include/gladys/nav_graph.hpp
+++ include/gladys/nav_graph.hpp
@@ -13,13 +13,15 @@
 #include <array>
 #include <vector>
 #include <string>
+
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/astar_search.hpp>
 #include <boost/graph/graphviz.hpp>
 
-/* TODO? Boost.Geometry (since libboost1.47, Ubuntu 12.04 = libboost1.46)
-include <boost/geometry/geometries/point_xy.hpp>
-boost::geometry::model::d2::point_xy<double>
-*/
+#include <boost/geometry/geometries/point_xy.hpp>
+/* Boost.Geometry (since libboost1.47, Ubuntu 12.04 = libboost1.46) */
 
 #include "gladys/weight_map.hpp"
 
@@ -31,8 +33,8 @@ typedef std::array<double, 3> point_xyz_t; // XYZ
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
     boost::property<boost::vertex_name_t, point_xy_t>,
     boost::property<boost::edge_weight_t, float> > graph_t;
-typedef boost::graph_traits<graph_t>::vertex_descriptor vertex_t;
-typedef boost::graph_traits<graph_t>::edge_descriptor edge_t;
+typedef graph_t::vertex_descriptor vertex_t;
+typedef graph_t::edge_descriptor edge_t;
 typedef std::map<point_xy_t, vertex_t> vertex_map_t;
 
 inline std::string to_string(const point_xy_t& value ) {
@@ -44,6 +46,12 @@ inline std::string to_string(const point_xyz_t& value ) {
     return std::to_string(value[0]) + "," +
            std::to_string(value[1]) + "," +
            std::to_string(value[2]);
+}
+
+inline double distance_sq(const point_xy_t& pA, const point_xy_t& pB) {
+    double x = pA[0] - pB[0];
+    double y = pA[1] - pB[1];
+    return x*x + y*y;
 }
 
 /*
@@ -72,6 +80,10 @@ public:
     // vertices
     vertex_t get_vertex(const double& x, const double &y) {
         point_xy_t p = {x, y};
+        return get_vertex(p);
+    }
+
+    vertex_t get_vertex(const point_xy_t& p) {
         vertex_map_t::iterator it = vertices.find(p);
         if (it != vertices.end())
             return it->second;
@@ -79,6 +91,30 @@ public:
         vertex_t v = boost::add_vertex(p, g);
         vertices[p] = v;
         return v;
+    }
+
+    vertex_t get_closest_vertex(const point_xy_t& p) {
+        vertex_map_t::iterator it = vertices.find(p);
+        if (it != vertices.end())
+            return it->second;
+
+        // else find closest vertex TODO optimize (scan all points)
+        vertex_t closest_v;
+        double tmp, closest_d = 1E+37;
+        for (auto& kv : vertices) {
+            tmp = distance_sq(p, kv.first);
+            if (tmp < closest_d) {
+                closest_v   = kv.second;
+                closest_d = tmp;
+            }
+        }
+        return closest_v;
+    }
+
+    void bfs(const point_xy_t& p) {
+        boost::bfs_visitor<boost::null_visitor> vis; // TODO visitor get path
+        boost::breadth_first_search(g, get_closest_vertex(p), boost::visitor(vis));
+        // return TODO path from visitor
     }
 
     bool is_obstacle(float weight) {
