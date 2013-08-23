806e5b4b779807a9bb2622a0029248cda9373b83
Pierrick Koch <pierrick.koch@gmail.com>
2013-08-23 11:01:01 +0200

    [ci] update travis w/python site pkg

--- .travis.bash
+++ .travis.bash
@@ -8,6 +8,8 @@ echo "Download CMake GDAL and Boost (build w/$n cores)"
 
 mkdir devel
 export DEVEL_ROOT=$(pwd)/devel
+export PYTHON_DEV=$DEVEL_ROOT/lib/python/site-packages
+export PYTHONPATH=$PYTHONPATH:$PYTHON_DEV
 #export BOOST_ROOT=$DEVEL_ROOT # used by FindBoost.cmake
 #(wget -q http://sf.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.bz2
 #tar jxf boost_1_54_0.tar.bz2 && cd boost_1_54_0 && ./bootstrap.sh --prefix=$BOOST_ROOT
@@ -22,7 +24,7 @@ echo "Build, test and install"
 
 set -e # exit on error
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$DEVEL_ROOT ..
+cmake -DCMAKE_INSTALL_PREFIX=$DEVEL_ROOT -DPYTHON_SITE_PACKAGES=$PYTHON_DEV ..
 make -j$n
 make test
 make install
