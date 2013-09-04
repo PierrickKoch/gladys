99fdb40133c81175b2144862c0990c497d4bb810
Pierrick Koch <pierrick.koch@gmail.com>
2013-09-04 10:40:30 +0200

    [python] cleanup gladys display

--- python/gladys_display.py
+++ python/gladys_display.py
@@ -20,16 +20,20 @@ class ImageLabel(QtGui.QLabel):
     pixmap = QtCore.Signal(QtGui.QImage)
     def __init__(self):
         QtGui.QLabel.__init__(self)
-        self.path = None
+        self.path = QtGui.QPolygonF()
         self.pixmap.connect(self._set_image)
     def _set_image(self, image):
         self.setPixmap(QtGui.QPixmap.fromImage(image))
+    def paint_path(self, path):
+        self.path = QtGui.QPolygonF()
+        for x, y in path:
+            self.path.append(QtCore.QPointF(x, y))
+        self.repaint()
     def paintEvent(self, event):
         QtGui.QLabel.paintEvent(self, event)
-        if self.path:
-            painter = QtGui.QPainter(self)
-            painter.setPen(QtGui.QPen(QtCore.Qt.black, 5))
-            painter.drawPoints(self.path)
+        painter = QtGui.QPainter(self)
+        painter.setPen(QtGui.QPen(QtCore.Qt.black, 5))
+        painter.drawPoints(self.path)
 
 def get_bgra32(b=0,g=0,r=0,a=0):
     return struct.pack('4B', int(b*255), int(g*255), int(r*255), int(a*255))
@@ -46,7 +50,7 @@ class MainWindow(QtGui.QMainWindow):
         # image viewer
         self.image_label = ImageLabel()
         self.setCentralWidget(self.image_label)
-        self.setWindowTitle("gladys display")
+        self.setWindowTitle("Gladys display")
         self.resize(200, 200)
         self.show()
         self.fregion = argv[1]
@@ -55,21 +59,18 @@ class MainWindow(QtGui.QMainWindow):
         self.bind(QtCore.Qt.Key_Escape, self.close)
         self.bind(QtCore.Qt.Key_Space,  self.start_search)
         self.bind(QtCore.Qt.Key_C,      self.clear_points)
-        # time load_nav_graph
+        print("Welcome to Gladys display. Loading graph...")
+        # start to load the graph in 50ms
         QtCore.QTimer.singleShot(50, self.load_nav_graph)
-    def show_region(self, width, height, fdata):
+    def show_f2rgb(self, width, height, fdata):
         buff = []
         fmin = min(fdata)
         diff = max(fdata) - fmin
-        print("min: %f, diff: %f" % (fmin, diff))
+        print("Display Float to RGB. min: %f, diff: %f" % (fmin, diff))
         if not diff: # useless
-            print("max == min (monochrome) : useless")
+            print("[warning] max == min (monochrome) : useless")
             return
         for fpx in fdata:
-            #try:
-            #    int(fpx)
-            #except: # if is NaN or inf
-            #    fpx = 255
             hue = (fpx - fmin) / diff
             buff.append(float_to_bgra32(hue))
         imageARGB32 = b''.join(buff)
@@ -77,22 +78,21 @@ class MainWindow(QtGui.QMainWindow):
     def load_nav_graph(self):
         self.ng = gladys.nav_graph(self.fregion, self.frobot)
         re = self.ng.get_map().get_region()
-        self.show_region(re.get_width(), re.get_height(), re.get_band('FLAT'))
+        print("Graph loaded. Display the FLAT layer of the region map")
+        self.show_f2rgb(re.get_width(), re.get_height(), re.get_band('FLAT'))
     def update_image(self, width, height, imageARGB32):
         image = QtGui.QImage(imageARGB32, width, height, QtGui.QImage.Format_ARGB32)
         self.image_label.pixmap.emit( image.copy() )
     def update_path(self, path):
-        qp = QtGui.QPolygonF(len(path))
-        for x, y in path:
-            qp.append(QtCore.QPointF(x, y))
-        self.image_label.path = qp
+        self.image_label.paint_path(path)
 
     def search(self):
         if not self.ng:
-            print("not ready for search (no graph)")
-        print("search: %s" % str((self.start, self.end)) )
+            print("[error] not ready for search (no graph)")
+            return
+        print("Search a path between %s" % str([self.start, self.end]) )
         pp = self.ng.search(self.start, self.end)
-        print("path: %s" % str(pp) )
+        print("Got a path of %i points" % len(pp) )
         self.update_path(pp)
         self.start = self.end = None
 
@@ -101,6 +101,7 @@ class MainWindow(QtGui.QMainWindow):
 
     def start_search(self):
         if self.start and self.end:
+            # start search in 50ms
             QtCore.QTimer.singleShot(50, self.search)
         else:
             print("select start and end points")
@@ -125,7 +126,6 @@ class MainWindow(QtGui.QMainWindow):
                 self.end = None
             else:
                 print("end = %s" % str(self.end) )
-        self.image_label.repaint()
 
 def main(argv=[]):
     if len(argv) < 3:
