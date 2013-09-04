1e04950f7c5ee35927f0c6267a056806d6edd556
Pierrick Koch <pierrick.koch@gmail.com>
2013-09-04 15:03:46 +0200

    [python] gladys display time load and search

--- python/gladys_display.py
+++ python/gladys_display.py
@@ -17,12 +17,10 @@ except ImportError:
     import sys; sys.exit(1)
 
 class ImageLabel(QtGui.QLabel):
-    pixmap = QtCore.Signal(QtGui.QImage)
     def __init__(self):
         QtGui.QLabel.__init__(self)
         self.path = QtGui.QPolygonF()
-        self.pixmap.connect(self._set_image)
-    def _set_image(self, image):
+    def set_image(self, image):
         self.setPixmap(QtGui.QPixmap.fromImage(image))
     def paint_path(self, path):
         self.path = QtGui.QPolygonF()
@@ -47,42 +45,61 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, argv):
         super(MainWindow, self).__init__()
         self.ng = self.start = self.end = None
+
         # image viewer
         self.image_label = ImageLabel()
         self.setCentralWidget(self.image_label)
+
         self.setWindowTitle("Gladys display")
         self.resize(200, 200)
-        self.show()
+
         self.fregion = argv[1]
         self.frobot = argv[2]
+
+        # key bindings
         self._bindings = {}
         self.bind(QtCore.Qt.Key_Escape, self.close)
         self.bind(QtCore.Qt.Key_Space,  self.start_search)
         self.bind(QtCore.Qt.Key_C,      self.clear_points)
+
         print("Welcome to Gladys display. Loading graph...")
         # start to load the graph in 50ms
         QtCore.QTimer.singleShot(50, self.load_nav_graph)
+
     def show_f2rgb(self, width, height, fdata):
         buff = []
         fmin = min(fdata)
         diff = max(fdata) - fmin
         print("Display Float to RGB. min: %f, diff: %f" % (fmin, diff))
+
         if not diff: # useless
             print("[warning] max == min (monochrome) : useless")
             return
+
         for fpx in fdata:
             hue = (fpx - fmin) / diff
             buff.append(float_to_bgra32(hue))
+
         imageARGB32 = b''.join(buff)
         self.update_image(width, height, imageARGB32)
-    def load_nav_graph(self):
-        self.ng = gladys.nav_graph(self.fregion, self.frobot)
+
+    def show_region(self):
+        t_start = time.time()
         re = self.ng.get_map().get_region()
-        print("Graph loaded. Display the FLAT layer of the region map")
         self.show_f2rgb(re.get_width(), re.get_height(), re.get_band('FLAT'))
+        print("Display the FLAT layer of the region map (%.3fs)" \
+            % (time.time() - t_start) )
+
+    def load_nav_graph(self):
+        t_start = time.time()
+        self.ng = gladys.nav_graph(self.fregion, self.frobot)
+        print("Graph loaded (%.3fs)" % (time.time() - t_start) )
+        self.show_region()
+
     def update_image(self, width, height, imageARGB32):
         image = QtGui.QImage(imageARGB32, width, height, QtGui.QImage.Format_ARGB32)
-        self.image_label.pixmap.emit( image.copy() )
+        self.image_label.set_image( image )
+
     def update_path(self, path):
         self.image_label.paint_path(path)
 
@@ -90,9 +107,12 @@ class MainWindow(QtGui.QMainWindow):
         if not self.ng:
             print("[error] not ready for search (no graph)")
             return
+
         print("Search a path between %s" % str([self.start, self.end]) )
+        t_start = time.time()
         pp = self.ng.search(self.start, self.end)
-        print("Got a path of %i points" % len(pp) )
+        print("Got a path of %i points (%.3fs)" % ( len(pp), \
+            (time.time() - t_start) ) )
         self.update_path(pp)
         self.start = self.end = None
 
@@ -105,9 +125,11 @@ class MainWindow(QtGui.QMainWindow):
             QtCore.QTimer.singleShot(50, self.search)
         else:
             print("select start and end points")
+
     def clear_points(self):
         self.start = self.end = None
         print("clear start and end")
+
     def close(self):
         QtGui.QMainWindow.close(self)
 
@@ -115,13 +137,15 @@ class MainWindow(QtGui.QMainWindow):
     def keyPressEvent(self, event):
         if event.key() in self._bindings:
             self._bindings[event.key()]()
+
     # PySide.QtGui.QWidget.mousePressEvent(event)
     def mousePressEvent(self, event):
+        pose = event.pos()
         if not self.start:
-            self.start = ( event.x(), event.y() )
+            self.start = ( pose.x(), pose.y() )
             print("start = %s" % str(self.start) )
         if not self.end:
-            self.end = ( event.x(), event.y() )
+            self.end = ( pose.x(), pose.y() )
             if self.end == self.start:
                 self.end = None
             else:
@@ -132,7 +156,8 @@ def main(argv=[]):
         print("usage: %s region.tif robot.json"%argv[0])
         return 1
     app = QtGui.QApplication(argv)
-    mw = MainWindow(argv)
+    mww = MainWindow(argv)
+    mww.show()
     return app.exec_()
 
 if __name__ == '__main__':
