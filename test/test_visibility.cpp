46e7277e7cd2cf6723fab24d473a4670d2396b58
Cyril Robin <cyril.robin@laas.fr>
2013-09-17 15:31:22 +0200

    [test] update visibility unit test
    Take into accourt the unknown/known state for visibility.

--- test/test_visibility.cpp
+++ test/test_visibility.cpp
@@ -37,16 +37,24 @@ BOOST_AUTO_TEST_CASE( test_visibility_map )
 
     // create a dtm map (GeoTiff image)
     gladys::gdal dtm;
-    dtm.set_size(1, 9, 9);
+    dtm.set_size(2, 9, 9);
     // add a small wall in the middle of the map
     dtm.bands_name[0] = "Z_MAX";
     dtm.get_band("Z_MAX").assign(9*9, 0.5);
     for (int i=0 ; i<9 ; i++ )
         dtm.get_band("Z_MAX")[ 5 + i*9 ] = 1.3;
-    // two special points to observe
+    // two special points to observe + the observer point
     dtm.get_band("Z_MAX")[ 8 + 0*9 ] = 1.9;
     dtm.get_band("Z_MAX")[ 8 + 8*9 ] = 1.1;
     dtm.get_band("Z_MAX")[ 0 + 5*9 ] = 0.6;
+    // add a small band of never-observed points
+    dtm.bands_name[1] = "N_POINTS";
+    dtm.get_band("N_POINTS").assign(9*9, 5.);
+    for (int i=0 ; i<9 ; i++ )
+        dtm.get_band("N_POINTS")[ 3 + i*9 ] = 0.0;
+    // one special point to observe
+    dtm.get_band("N_POINTS")[ 8 + 5*9 ] = 0.0;
+
     dtm.save(dtm_path);
 
     //// create a visibility map from the dtm
@@ -57,6 +65,7 @@ BOOST_AUTO_TEST_CASE( test_visibility_map )
     gladys::point_xy_t s  = {0, 5}; // sensor
     gladys::point_xy_t t1 = {8, 0}; // target 1 -- visible
     gladys::point_xy_t t2 = {8, 8}; // target 2 -- invisible
+    gladys::point_xy_t t3 = {8, 5}; // target 3 -- invisible
 
     bool b ;
     b = vm.is_visible( s, t1) ;
@@ -65,6 +74,10 @@ BOOST_AUTO_TEST_CASE( test_visibility_map )
 
     b = vm.is_visible( s, t2) ;
     BOOST_TEST_MESSAGE( "t2 visible from s ? " + b );
+    BOOST_CHECK_EQUAL( b, false );
+
+    b = vm.is_visible( s, t3) ;
+    BOOST_TEST_MESSAGE( "t3 visible from s ? " + b );
     BOOST_CHECK_EQUAL( b, false );
 
 }
