7eb5848175fafa6f0d5ca89f90959dc141360cd1
Pierrick Koch <pierrick.koch@gmail.com>
2013-09-24 23:05:07 +0200

    [ci] clone gdalwrap for travis test

--- .travis.bash
+++ .travis.bash
@@ -4,15 +4,27 @@ echo "======"
 uname -a # show kernel info
 n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
 
+SRC_DIR=$(pwd)
 mkdir devel
 export DEVEL_ROOT=$(pwd)/devel
 export PYTHON_DEV=$DEVEL_ROOT/lib/python/site-packages
+export PKG_CONFIG_PATH=$DEVEL_ROOT/lib/pkgconfig:$PKG_CONFIG_PATH
 export PYTHONPATH=$PYTHONPATH:$PYTHON_DEV
 #export BOOST_ROOT=$DEVEL_ROOT # used by FindBoost.cmake
 #(wget -q http://sf.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.bz2
 #tar jxf boost_1_54_0.tar.bz2 && cd boost_1_54_0 && ./bootstrap.sh --prefix=$BOOST_ROOT
 #./b2 -j$n install )& boostpid=$!
 #wait $boostpid && grep "VERSION" $BOOST_ROOT/include/boost/version.hpp
+
+echo "==================================="
+echo "Install gdalwrap"
+
+git clone https://github.com/pierriko/gdalwrap.git && cd gdalwrap
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$DEVEL_ROOT ..
+make -j$n && make install
+
+cd $SRC_DIR
 
 echo "==================================="
 echo "Build (w/$n cores) test and install"
