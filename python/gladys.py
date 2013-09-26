92c52b35d163e0cced5d6ebe133ebd738bc85f39
Pierrick Koch <pierrick.koch@gmail.com>
2013-09-26 23:23:25 +0200

    [python] gladyspoints w/custom origin

--- python/gladys.py
+++ python/gladys.py
@@ -11,3 +11,14 @@ def point_pix2utm(gdal, x, y):
 def point_utm2pix(gdal, x, y):
     return ((x - gdal.get_utm_pose_x()) / gdal.get_scale_x(),
             (y - gdal.get_utm_pose_y()) / gdal.get_scale_y())
+
+def point_pix2custom(gdal, x, y):
+    p = point_pix2utm(gdal, x, y)
+    return (p[0] - gdal.get_custom_x_origin(),
+            p[1] - gdal.get_custom_y_origin())
+
+def point_custom2pix(gdal, x, y):
+    return point_utm2pix( gdal,
+            x + gdal.get_custom_x_origin(),
+            y + gdal.get_custom_y_origin() )
+
