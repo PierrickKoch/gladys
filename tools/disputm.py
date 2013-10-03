124a4415916aee794b172b91569e9913083e219c
Pierrick Koch <pierrick.koch@gmail.com>
2013-10-03 11:09:54 +0200

    [tool] display UTM -> Custom : work on zoom

--- tools/disputm.py
+++ tools/disputm.py
@@ -18,33 +18,51 @@ def draw_path(paintable, path, sx=1.0, sy=1.0):
     painter = QtGui.QPainter(paintable)
     painter.scale(sx, sy)
     painter.setPen(QtGui.QPen(QtCore.Qt.green, 4))
-    # TODO draw path
+    # draw path
     painter.drawPolyline(path)
     painter.setPen(QtGui.QPen(QtCore.Qt.red, 4))
     for num, point in enumerate(path):
         painter.drawText(point, "%i"%num)
     painter.end()
 
+def draw_points(paintable, points, sx=1.0, sy=1.0):
+    painter = QtGui.QPainter(paintable)
+    painter.scale(sx, sy)
+    painter.setPen(QtGui.QPen(QtCore.Qt.red, 4))
+    # draw points
+    painter.drawPoints(points)
+    painter.end()
+
 class ImageLabel(QtGui.QLabel):
     def __init__(self):
         QtGui.QLabel.__init__(self)
-        self.path = QtGui.QPolygonF()
-        self.scale = 1.0
+        self.path   = QtGui.QPolygonF()
+        self.points = QtGui.QPolygonF()
+        self.scale  = 1.0
     def set_image(self, image):
         self.image = image
         size = image.size() * self.scale
         self.setPixmap(QtGui.QPixmap.fromImage( image.scaled(size) ))
+    def add_point(self, x, y):
+        self.points.append(QtCore.QPointF(x, y))
+        self.repaint()
+    def clear_points(self):
+        self.points = QtGui.QPolygonF()
+        self.repaint()
     def paint_path(self, path):
         self.path = QtGui.QPolygonF([ QtCore.QPointF(x, y) for x, y in path ])
         self.repaint()
-    def paintEvent(self, event):
-        QtGui.QLabel.paintEvent(self, event)
-        draw_path(self, self.path, self.scale, self.scale)
-    def set_scale(self, scale=1.0):
-        self.scale = scale
+    def apply_scale(self, scale=None):
+        if scale:
+            self.scale = scale
         self.set_image(self.image)
         self.repaint()
-
+    def paintEvent(self, event):
+        # override QtGui.QLabel.paintEvent
+        # called by QtGui.QLabel.repaint, in the GUI loop
+        QtGui.QLabel.paintEvent(self, event)
+        draw_path  (self, self.path,   self.scale, self.scale)
+        draw_points(self, self.points, self.scale, self.scale)
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, argv):
@@ -80,10 +98,10 @@ class MainWindow(QtGui.QMainWindow):
         self.bind(QtCore.Qt.Key_A,      self.zoom_out)
 
     def zoom_in(self):
-        self.image_label.set_scale(2.0)
+        self.image_label.apply_scale(2.0)
 
     def zoom_out(self):
-        self.image_label.set_scale(0.5)
+        self.image_label.apply_scale(0.5)
 
     def point_pix2utm(self, point):
         return gladys.point_pix2utm(self.image_gdal, *point)
@@ -99,10 +117,10 @@ class MainWindow(QtGui.QMainWindow):
             custom = self.point_pix2custom(point)
             utm_coordinates.append(utm)
             custom_coordinates.append(custom)
-            print("%s -> %s" % ( str(point), str(utm) ) )
-        for x, y in custom_coordinates:
-            print("%f %f" % (x, y) )
-        self.display_path()
+            print("[%5i, %5i] -> [%12.3f, %12.3f] -> [%9.3f, %9.3f]" % \
+                  ( point[0], point[1], utm[0], utm[1], custom[0], custom[1] ) )
+        # display path
+        self.image_label.paint_path(self.points_pix)
         self.save_custom(custom_coordinates)
         self.save_image()
 
@@ -112,9 +130,6 @@ class MainWindow(QtGui.QMainWindow):
         draw_path(image, path)
         image.save('/tmp/path.png')
 
-    def display_path(self):
-        self.image_label.paint_path(self.points_pix)
-
     def save_custom(self, coord):
         with open('/tmp/path.txt', 'w') as f:
             for x, y in coord:
@@ -122,7 +137,8 @@ class MainWindow(QtGui.QMainWindow):
 
     def clear_points(self):
         self.points_pix = []
-        print("clear points")
+        self.image_label.paint_path([])
+        self.image_label.clear_points()
 
     def bind(self, key, func):
         self._bindings[key] = func
@@ -132,6 +148,12 @@ class MainWindow(QtGui.QMainWindow):
         if event.key() in self._bindings:
             self._bindings[event.key()]()
 
+    # PySide.QtGui.QWidget.wheelEvent(event)
+    def wheelEvent(self, event):
+        # TODO
+        self.image_label.scale += event.delta() / 400.0;
+        self.image_label.apply_scale()
+
     # PySide.QtGui.QWidget.mousePressEvent(event)
     def mousePressEvent(self, event):
         pose = event.pos()
@@ -139,7 +161,7 @@ class MainWindow(QtGui.QMainWindow):
         if self.points_pix and self.points_pix[-1] == point:
             return # skip double click
         self.points_pix.append(point)
-        print("new point: %s" % str(point) )
+        self.image_label.add_point(*point)
 
 def main(argv=[]):
     if len(argv) < 2:
