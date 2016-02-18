92503655b4bf4f0890f4fb5d71c8f2dddbc2f1f1
Pierrick Koch <pierrick.koch@gmail.com>
2016-02-18 13:47:26 +0100

    [python] add missing utm-custom convert

--- python/gladys.py
+++ python/gladys.py
@@ -1,5 +1,13 @@
 from libgladys_python import *
 
+def point_custom2utm(gdal, x, y):
+    return (x + gdal.get_custom_x_origin(),
+            y + gdal.get_custom_y_origin())
+
+def point_utm2custom(gdal, x, y):
+    return (x - gdal.get_custom_x_origin(),
+            y - gdal.get_custom_y_origin())
+
 def point_pix2utm(gdal, x, y):
     return ( x * gdal.get_scale_x() + gdal.get_utm_pose_x() ,
              y * gdal.get_scale_y() + gdal.get_utm_pose_y() )
@@ -9,12 +17,7 @@ def point_utm2pix(gdal, x, y):
             (y - gdal.get_utm_pose_y()) / gdal.get_scale_y())
 
 def point_pix2custom(gdal, x, y):
-    p = point_pix2utm(gdal, x, y)
-    return (p[0] - gdal.get_custom_x_origin(),
-            p[1] - gdal.get_custom_y_origin())
+    return point_utm2custom(gdal, *point_pix2utm(gdal, x, y))
 
 def point_custom2pix(gdal, x, y):
-    return point_utm2pix( gdal,
-            x + gdal.get_custom_x_origin(),
-            y + gdal.get_custom_y_origin() )
-
+    return point_utm2pix(gdal, *point_custom2utm(gdal, x, y))
