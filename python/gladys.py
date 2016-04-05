f415b7d8953e2b1489246556d7de812a1bffe458
Pierrick Koch <pierrick.koch@gmail.com>
2016-04-05 15:44:52 +0200

    [python] add Gladys class from Atlaas

--- python/gladys.py
+++ python/gladys.py
@@ -21,3 +21,26 @@ def point_pix2custom(gdal, x, y):
 
 def point_custom2pix(gdal, x, y):
     return point_utm2pix(gdal, *point_custom2utm(gdal, x, y))
+
+class Gladys:
+    def __init__(self, region, robot):
+        self.ctmap = costmap(region, robot)
+        self.graph = nav_graph(self.ctmap)
+        self.gdmap = self.ctmap.get_map()
+    def u2p(self, x, y):
+        return point_utm2pix(self.gdmap, x, y)
+    def p2u(self, x, y):
+        return point_pix2utm(self.gdmap, x, y)
+    def c2p(self, x, y):
+        return point_custom2pix(self.gdmap, x, y)
+    def p2c(self, x, y):
+        return point_pix2custom(self.gdmap, x, y)
+    def c2u(self, x, y):
+        return point_custom2utm(self.gdmap, x, y)
+    def u2c(self, x, y):
+        return point_utm2custom(self.gdmap, x, y)
+    def path(self, a, b):
+        # A* a, b
+        res_utm, cost = self.graph.search_with_cost(self.c2u(*a), self.c2u(*b))
+        res = [self.u2c(x, y) for x, y in res_utm]
+        return res, cost
